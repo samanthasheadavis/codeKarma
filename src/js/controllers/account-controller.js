a571d8ac064cdc2019d597d60d330fc56a81189c
Samantha Davis <samantha.shea.davis@gmail.com>
2016-11-16 14:04:28 -0500

    working on navbar issues

--- src/js/controllers/account-controller.js
+++ src/js/controllers/account-controller.js
@@ -1,5 +1,54 @@
-angular.module('codeKarmaApp').controller('AccountController', function($state, $location) {
-  this.message = "in AccountController";
-  this.url = $location.url();
-  console.log(this.url);
+angular.module('codeKarmaApp').controller('AccountController', function($state, $location, $rootScope) {
+
+  var hideDev = true;
+  var hideClient = true;
+
+  // this.view = function() {
+    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+        this.url = toState.url;
+        if (this.url === 'developer/all-projects' || this.url === 'developer/community-feed' || this.url === 'developer/dashboard' || this.url === 'developer/projects') {
+          console.log('dev');
+            hideDev = true;
+            // return hideDev;
+        } else if (this.url === 'client/dashboard' || this.url === 'client/projects' || this.url === 'client/add-project') {
+          console.log('client');
+            hideClient = true;
+            // return hideClient;
+        }
+    });
+  // };
+
+      return {
+        hideDev: hideDev,
+        hideClient: hideClient
+      };
+
+
+
+
+
+
+    // var hideDev = true;
+    // var hideClient = true;
+    //
+    //   $(window).on('hashchange', function() {
+    //     console.log('in');
+    //
+    //       if ($location.url() === '/developer/all-projects' || $location.url() === '/developer/community-feed' ||  $location.url() === '/developer/dashboard' || $location.url() === '/developer/projects') {
+    //         hideDev = true;
+    //         return hideDev;
+    //       } else if ($location.url() === '/client/dashboard' || $location.url() === '/client/projects' || $location.url() === '/client/add-project'){
+    //         hideClient = true;
+    //         return hideClient;
+    //       }
+    //       // else if ($location.url() === '/') {
+    //       //   hideDev = true;
+    //       //   return this.hide;
+    //       // }
+    //   });
+    //
+    // return {
+    //   hideDev: hideDev,
+    //   hideClient: hideClient
+    // };
 });
