267f71ca223601bc70071c41654e0d468a58c485
Samantha Davis <samantha.shea.davis@gmail.com>
2016-11-16 17:26:49 -0500

    updated dev dashboard so user can update skills, name and link dynamically

--- src/js/controllers/dev-dashboard-controller.js
+++ src/js/controllers/dev-dashboard-controller.js
@@ -1,9 +1,18 @@
 angular.module('codeKarmaApp').controller('DevDashboardController', function($state) {
 
+  this.devName = 'Samantha Davis';
+  this.devLink = 'github.com/yourName';
+  this.skills = ['boiling water', 'napping'];
+
   this.updateInfo=function(value) {
-    console.log(value);
+    if (value === "skills") {
+      this.skills.push(this.newSkill);
+      this.newSkill = '';
+    }
+
     this.showLinkEdit = false;
     this.showNameEdit = false;
+    this.showSkillEdit = false;
   };
 
 });
