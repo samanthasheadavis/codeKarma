b0cd475691bdc6a8b1f20d9447c9e5b89a6bb341
Samantha Davis <samantha.shea.davis@gmail.com>
2016-11-16 15:03:46 -0500

    navbar refresh fix

--- src/js/controllers/account-controller.js
+++ src/js/controllers/account-controller.js
@@ -1,5 +1,17 @@
 angular.module('codeKarmaApp').controller('AccountController', function($state, $location, $rootScope) {
 
+    this.refreshView = function() {
+        this.url = $location.url();
+
+        if (this.url === '/developer/all-projects' || this.url === '/developer/community-feed' || this.url === '/developer/dashboard' || this.url === '/developer/projects') {
+            this.hideDev = true;
+        } else if (this.url === '/client/dashboard' || this.url === '/client/projects' || this.url === '/client/add-project') {
+            this.hideClient = true;
+        } else if (this.url === '/') {
+            this.hideNav = true;
+        }
+    };
+
     this.views = function(view) {
         console.log('in');
         if (view === 'developer/all-projects' || view === 'developer/community-feed' || view === 'developer/dashboard' || view === 'developer/projects') {
@@ -13,4 +25,6 @@ angular.module('codeKarmaApp').controller('AccountController', function($state, 
             this.hideDev = false;
         }
     };
+
+    this.refreshView();
 });
