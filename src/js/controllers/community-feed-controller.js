50bd07725cd63ae88de63ae8f5b709b38d95dfdb
Samantha Davis <samantha.shea.davis@gmail.com>
2016-11-16 16:06:50 -0500

    added commFeed functionality: liking and commenting

--- src/js/controllers/community-feed-controller.js
+++ src/js/controllers/community-feed-controller.js
@@ -1,4 +1,20 @@
 angular.module('codeKarmaApp').controller('CommunityFeedController', function($state) {
 
-  
+    this.post = '';
+    this.response = '';
+    this.likes = 12;
+
+    this.addPost = function() {
+        console.log(this.post);
+    };
+
+    this.addResponse = function(response) {
+      var newResponse = $('<p>').attr('class', 'response').html(response).appendTo('.responsesContainer');
+    };
+
+    this.updateLikes = function(likes) {
+      console.log(likes);
+      likes++;
+      this.likes = likes;
+    };
 });
