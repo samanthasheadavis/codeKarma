00677408720c672360283157ea7c400d7e09d001
Samantha Davis <samantha.shea.davis@gmail.com>
2016-11-16 14:11:08 -0500

    navbar work

--- src/js/controllers/account-controller.js
+++ src/js/controllers/account-controller.js
@@ -25,6 +25,36 @@ angular.module('codeKarmaApp').controller('AccountController', function($state, 
 
 
 
+      angular.module('codeKarmaApp').service('Nav', function($location, $rootScope) {
+
+        $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+            this.url = toState.url;
+            if (this.url === 'developer/all-projects' || this.url === 'developer/community-feed' || this.url === 'developer/dashboard' || this.url === 'developer/projects') {
+              console.log('dev');
+                hideDev = true;
+                return hideDev;
+            } else if (this.url === 'client/dashboard' || this.url === 'client/projects' || this.url === 'client/add-project') {
+              console.log('client');
+                hideClient = true;
+                return hideClient;
+            }
+        });
+
+          return {
+            hideDev: hideDev,
+            hideClient: hideClient
+          };
+
+  });
+
+
+
+
+
+
+
+
+
 
 
 
