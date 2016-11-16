6f69ed1011ff0caf046ffd5a8b169db567e31eb8
Samantha Davis <samantha.shea.davis@gmail.com>
2016-11-16 14:52:41 -0500

    navbar now showing only relevant links based on user role

--- src/js/controllers/account-controller.js
+++ src/js/controllers/account-controller.js
@@ -1,84 +1,17 @@
 angular.module('codeKarmaApp').controller('AccountController', function($state, $location, $rootScope) {
 
-  var hideDev = true;
-  var hideClient = true;
-
-  // this.view = function() {
-    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
-        this.url = toState.url;
-        if (this.url === 'developer/all-projects' || this.url === 'developer/community-feed' || this.url === 'developer/dashboard' || this.url === 'developer/projects') {
-          console.log('dev');
-            hideDev = true;
-            // return hideDev;
-        } else if (this.url === 'client/dashboard' || this.url === 'client/projects' || this.url === 'client/add-project') {
-          console.log('client');
-            hideClient = true;
-            // return hideClient;
+    this.views = function(view) {
+        console.log('in');
+        if (view === 'developer/all-projects' || view === 'developer/community-feed' || view === 'developer/dashboard' || view === 'developer/projects') {
+            this.hideDev = true;
+        } else if (view === 'client/dashboard' || view === 'client/projects' || view === 'client/add-project') {
+            this.hideClient = true;
+        } else if (view === 'home') {
+            this.hideNav = true;
+        } else if (view === 'all') {
+            this.hideClient = false;
+            this.hideDev = false;
         }
-    });
-  // };
+    };
 
-      return {
-        hideDev: hideDev,
-        hideClient: hideClient
-      };
-
-
-
-      angular.module('codeKarmaApp').service('Nav', function($location, $rootScope) {
-
-        $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
-            this.url = toState.url;
-            if (this.url === 'developer/all-projects' || this.url === 'developer/community-feed' || this.url === 'developer/dashboard' || this.url === 'developer/projects') {
-              console.log('dev');
-                hideDev = true;
-                return hideDev;
-            } else if (this.url === 'client/dashboard' || this.url === 'client/projects' || this.url === 'client/add-project') {
-              console.log('client');
-                hideClient = true;
-                return hideClient;
-            }
-        });
-
-          return {
-            hideDev: hideDev,
-            hideClient: hideClient
-          };
-
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-    // var hideDev = true;
-    // var hideClient = true;
-    //
-    //   $(window).on('hashchange', function() {
-    //     console.log('in');
-    //
-    //       if ($location.url() === '/developer/all-projects' || $location.url() === '/developer/community-feed' ||  $location.url() === '/developer/dashboard' || $location.url() === '/developer/projects') {
-    //         hideDev = true;
-    //         return hideDev;
-    //       } else if ($location.url() === '/client/dashboard' || $location.url() === '/client/projects' || $location.url() === '/client/add-project'){
-    //         hideClient = true;
-    //         return hideClient;
-    //       }
-    //       // else if ($location.url() === '/') {
-    //       //   hideDev = true;
-    //       //   return this.hide;
-    //       // }
-    //   });
-    //
-    // return {
-    //   hideDev: hideDev,
-    //   hideClient: hideClient
-    // };
 });
