9420ddc99284b25518c2bdbe71109356fc6f4be7
Lindsey Adams <linzma87@gmail.com>
2016-11-17 09:02:21 -0500

    re-routing continue buttons to heroku

--- src/js/controllers/client-projects-controller.js
+++ src/js/controllers/client-projects-controller.js
@@ -1,6 +1,8 @@
 angular.module('codeKarmaApp').controller('ClientProjectsController', function($state) {
   this.message = "in ClientProjectsController";
 
+
+
   // collect edited data
 
   // post updated object to backend
--- src/js/controllers/dev-projects-controller.js
+++ src/js/controllers/dev-projects-controller.js
@@ -1,6 +1,15 @@
-angular.module('codeKarmaApp').controller('DevProjectsController', function($state) {
+angular.module('codeKarmaApp').controller('DevProjectsController', function($state, $scope) {
 
   this.complete = false;
+
+  $scope.slider_floor_ceil = {
+        value: 10,
+        options: {
+            floor: 0,
+            ceil: 100,
+            step: 10
+        }
+    };
 
   // update and post progress
 
@@ -8,24 +17,21 @@ angular.module('codeKarmaApp').controller('DevProjectsController', function($sta
 
     this.status = status;
 
+    // if progress < 100%, show "ask for help" button
+    // if progress === 100%, show "submit project" and disable est. completion date
+
     if (this.status !== "100") {
       this.complete = false;
     } else if (this.status === "100") {
       this.complete = true;
     }
 
-    console.log(this.status);
-    console.log(this.complete);
-    // $state.reload();
-
   };
+
+
 
   // update and post est. completion date
 
 
-  // if progress < 100%, show "ask for help" button
-
-
-  // if progress === 100%, show "submit project"
 
 });
