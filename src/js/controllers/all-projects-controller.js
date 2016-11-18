0534196b02aeeb28f836b082a5d0201c53d5b9e1
Lindsey Adams <linzma87@gmail.com>
2016-11-17 21:02:03 -0500

    get client info testing

--- src/js/controllers/all-projects-controller.js
+++ src/js/controllers/all-projects-controller.js
@@ -1,10 +1,15 @@
 angular.module('codeKarmaApp').controller('AllProjectsController', function($state, $scope) {
   $scope.details = false;
-  this.category = "bug fix";
+  this.category = "Bug Fix";
 
   $scope.toggleDetails = function () {
     $scope.details = !$scope.details;
   };
+
+
+  // get request to get project info
+
+
 
   // fork project function
 
@@ -13,6 +18,23 @@ angular.module('codeKarmaApp').controller('AllProjectsController', function($sta
   // add project info to user object
 
 
+  // determine icon to show on project - grab category(fix_type )
 
+  this.getIcon = function(responseObj) {
+
+    this.category = responseObj.fix_type;
+
+    if (this.category === "Bug Fix") {
+      this.img = "bugfix";
+      this.alt = "Icon Fair";
+    } else if (this.category === "Design Update") {
+      this.img = "design_update";
+      this.alt = "Oliviu Stoian";
+    } else if (this.category === "New Feature") {
+      this.img = "new_feature";
+      this.alt = "Phil Goodwin";
+    }
+
+  };
 
 });
