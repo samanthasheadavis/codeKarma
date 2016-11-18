d631f58889768edf5c3da5018aad3a2b13a5bbc4
Lindsey Adams <linzma87@gmail.com>
2016-11-18 10:07:37 -0500

    syntax fix on request service

--- src/js/services/request-service.js
+++ src/js/services/request-service.js
@@ -10,52 +10,52 @@ angular.module('codeKarmaApp').service('RequestService', function($http, $locati
         } else if (url.includes("Developer")) {
             $state.go('codeKarmaParent.devDashboard');
         }
-
-        function getToken() {
-            var url = $location.url();
-            token = url.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/)[0];
-            // 
-            // if (url.includes("redirect")) {
-                dashboardRedirect();
-            // } else {
-            //     return token;
-            // }
-        }
-
-        function getClient(callback) {
-            $http({
-                method: 'GET',
-                url: 'https://code-karma-api.herokuapp.com/clients/1?token=' + token,
-            }).then(callback, function errorCallback(response) {
-                return createUser(response.data.info);
-            });
-        }
-
-        function getDev(callback) {
-            $http({
-                method: 'GET',
-                url: 'https://code-karma-api.herokuapp.com/developers/2?token=' + token,
-            }).then(callback, function errorCallback(response) {
-                return createUser(response.data.info);
-            });
-        }
-
-        function createUser(response) {
-            this.currentUser = {
-                username: response.nickname,
-                name: response.name,
-                email: response.email,
-                image: response.image,
-                github: response.urls.GitHub,
-            };
-            return this.currentUser;
-        }
-
-        return {
-            getClient: getClient,
-            getDev: getDev,
-            getToken: getToken
-        };
-
     }
+
+    function getToken() {
+        var url = $location.url();
+        token = url.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/)[0];
+
+        if (url.includes("redirect")) {
+        dashboardRedirect();
+        } else {
+            return token;
+        }
+    }
+
+    function getClient(callback) {
+        $http({
+            method: 'GET',
+            url: 'https://code-karma-api.herokuapp.com/clients/1?token=' + token,
+        }).then(callback, function errorCallback(response) {
+            return createUser(response.data.info);
+        });
+    }
+
+    function getDev(callback) {
+        $http({
+            method: 'GET',
+            url: 'https://code-karma-api.herokuapp.com/developers/2?token=' + token,
+        }).then(callback, function errorCallback(response) {
+            return createUser(response.data.info);
+        });
+    }
+
+    function createUser(response) {
+        this.currentUser = {
+            username: response.nickname,
+            name: response.name,
+            email: response.email,
+            image: response.image,
+            github: response.urls.GitHub,
+        };
+        return this.currentUser;
+    }
+
+    return {
+        getClient: getClient,
+        getDev: getDev,
+        getToken: getToken
+    };
+
 });
