5d896bfc344a76b125db5025af80d2f76ac376e5
Samantha Davis <samantha.shea.davis@gmail.com>
2016-11-18 10:10:53 -0500

    work on client add projects getToken

--- src/js/controllers/client-add-project-controller.js
+++ src/js/controllers/client-add-project-controller.js
@@ -1,29 +1,27 @@
-angular.module('codeKarmaApp').controller('ClientAddProjectController', function(RequestService, $state, $http) {
+angular.module('codeKarmaApp').controller('ClientAddProjectController', function($state, $http, RequestService) {
 
+    // collect form data and put into project object
 
-  // collect form data and put into project object
+    this.create = function(title, briefDescription, description, category, githubRepo) {
+        this.newProject = {
+            title: title,
+            brief_description: briefDescription,
+            description: description,
+            fix_type: category,
+            github_repo_url: githubRepo,
+            active_site_url: "grab from client object"
+        };
 
-  this.create = function(title, briefDescription, description, category, githubRepo) {
-    this.newProject = {
-      title: title,
-      brief_description: briefDescription,
-      description: description,
-      fix_type: category,
-      github_repo_url: githubRepo,
-      active_site_url: "grab from client object"
+        console.log(this.newProject);
+        // $state.go('codeKarmaParent.clientProjects');
+
     };
 
-    console.log(this.newProject);
-    $state.go('codeKarmaParent.clientProjects');
+    // post project object to backend
 
-  };
+        this.token = RequestService.getToken();
 
-  // post project object to backend
-this.getToken = function() {
-  this.token = RequestService.getToken();
-  console.log(this.token);
+        // create post url
 
-  // create post url
-};
 
 });
