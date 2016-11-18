683df84d619c49fac3540a30181a87124cb820af
Lindsey Adams <linzma87@gmail.com>
2016-11-18 09:35:13 -0500

    move user build to request service

--- src/js/controllers/client-dashboard-controller.js
+++ src/js/controllers/client-dashboard-controller.js
@@ -11,28 +11,7 @@ angular.module('codeKarmaApp').controller('ClientDashboardController', function(
         this.showNameEdit = false;
     };
 
-    this.getUser = function() {
-
-        RequestService.getUser(function(response) {
-
-            console.log(response);
-
-            this.currentUser = {
-
-                username: response.data.info.nickname,
-                name: response.data.info.name,
-                email: response.data.info.email,
-                image: response.data.info.image,
-                github: response.data.info.urls.GitHub
-
-            };
-
-            console.log(this.currentUser);
-
-        });
-    };
-
-    this.getUser();
+    RequestService.getClient();
 
 
 });
--- src/js/services/request-service.js
+++ src/js/services/request-service.js
@@ -4,7 +4,6 @@ var token = '';
 
   function dashboardRedirect() {
     var url = $location.url();
-
     if (url.includes("Client")) {
       $state.go('codeKarmaParent.clientDashboard');
     } else if (url.includes("Developer")) {
@@ -15,22 +14,41 @@ var token = '';
   function getToken() {
     var url = $location.url();
     token = url.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/)[0];
-
     dashboardRedirect();
   }
 
-function getUser(callback) {
-
+function getClient(callback) {
   $http({
       method: 'GET',
       url: 'https://code-karma-api.herokuapp.com/clients/1?token=' + token,
   }).then(callback, function errorCallback(response) {
-      return response;
+      return createUser(response.data.info);
   });
 }
 
+function getDev(callback) {
+  $http({
+      method: 'GET',
+      url: 'https://code-karma-api.herokuapp.com/developers/2?token=' + token,
+  }).then(callback, function errorCallback(response) {
+      return createUser(response.data.info);
+  });
+}
+
+function createUser(response) {
+  this.currentUser = {
+      username: response.nickname,
+      name: response.name,
+      email: response.email,
+      image: response.image,
+      github: response.urls.GitHub
+  };
+  return this.currentUser;
+}
+
 return {
-    getUser: getUser,
+    getClient: getClient,
+    getDev: getDev,
     getToken: getToken
 };
 
