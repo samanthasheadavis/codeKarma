294be0a15bdf4cbd0f1419eee95e96d58f9df6ba
Lindsey Adams <linzma87@gmail.com>
2016-11-18 09:38:40 -0500

    modifying getToken to redirect or return token based on url

--- src/js/services/request-service.js
+++ src/js/services/request-service.js
@@ -1,37 +1,41 @@
 angular.module('codeKarmaApp').service('RequestService', function($http, $location, $state) {
 
-var token = '';
+    var token = '';
 
-  function dashboardRedirect() {
-    var url = $location.url();
+    function dashboardRedirect() {
+        var url = $location.url();
 
-    if (url.includes("Client")) {
-      $state.go('codeKarmaParent.clientDashboard');
-    } else if (url.includes("Developer")) {
-      $state.go('codeKarmaParent.devDashboard');
+        if (url.includes("Client")) {
+            $state.go('codeKarmaParent.clientDashboard');
+        } else if (url.includes("Developer")) {
+            $state.go('codeKarmaParent.devDashboard');
+        }
     }
-  }
 
-  function getToken() {
-    var url = $location.url();
-    token = url.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/)[0];
+    function getToken() {
+        var url = $location.url();
+        token = url.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/)[0];
 
-    dashboardRedirect();
-  }
+        if (url.includes("redirect")) {
+            dashboardRedirect();
+        } else {
+            return token;
+        }
+    }
 
-function getUser(callback) {
+    function getUser(callback) {
 
-  $http({
-      method: 'GET',
-      url: 'https://code-karma-api.herokuapp.com/clients/1?token=' + token,
-  }).then(callback, function errorCallback(response) {
-      return response;
-  });
-}
+        $http({
+            method: 'GET',
+            url: 'https://code-karma-api.herokuapp.com/clients/1?token=' + token,
+        }).then(callback, function errorCallback(response) {
+            return response;
+        });
+    }
 
-return {
-    getUser: getUser,
-    getToken: getToken
-};
+    return {
+        getUser: getUser,
+        getToken: getToken
+    };
 
 });
