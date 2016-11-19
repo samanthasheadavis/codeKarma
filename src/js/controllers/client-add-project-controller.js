80a359b637873a9d17d64cdfdff549f574d52a6b
Samantha Davis <samantha.shea.davis@gmail.com>
2016-11-19 16:27:37 -0500

    added test to this.geturl

--- src/js/controllers/client-add-project-controller.js
+++ src/js/controllers/client-add-project-controller.js
@@ -3,13 +3,14 @@ angular.module('codeKarmaApp').controller('ClientAddProjectController', function
 
   // this.url calls to the request service to get the correct url for posting to including the user id and token, and saves it in the controller variable this.url
 
-  this.url = function() {
+  this.getUrl = function() {
     this.url = RequestService.getClientUrl();
   };
 
     // collect form data and put into project object
 
     this.create = function(title, briefDescription, description, category, githubRepo) {
+      console.log("in create" + this.url);
         this.newProject = {
             title: title,
             brief_description: briefDescription,
@@ -28,5 +29,5 @@ angular.module('codeKarmaApp').controller('ClientAddProjectController', function
 
     };
 
-    this.url();
+    this.getUrl();
 });
