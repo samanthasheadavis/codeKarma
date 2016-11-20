54de7a71d5fe5eb161c80d6c913a6af5746b94c8
Samantha Davis <samantha.shea.davis@gmail.com>
2016-11-20 15:52:33 -0500

    client projects modal box styling

--- src/js/controllers/client-projects-controller.js
+++ src/js/controllers/client-projects-controller.js
@@ -1,33 +1,42 @@
 angular.module('codeKarmaApp').controller('ClientProjectsController', function($state, RequestService, $scope) {
-  this.message = "in ClientProjectsController";
+    this.message = "in ClientProjectsController";
 
-// Collect client projects
-  this.getProjects= function() {
-    this.url = RequestService.getClientProjectsUrl();
+    // Collect client projects
+    this.getProjects = function() {
+        this.url = RequestService.getClientProjectsUrl();
 
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": this.url,
-        "method": "GET"
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": this.url,
+            "method": "GET"
+        };
+
+        $.ajax(settings).done(function(response) {
+            $scope.projects = response;
+            $scope.$apply();
+        });
     };
 
-    $.ajax(settings).done(function(response) {
-      $scope.projects = response;
-        $scope.$apply();
-    });
-  };
+    // collect edited data
 
-  // collect edited data
+    this.updateInfo = function(project) {
+        project.showTitleEdit = !project.showTitleEdit;
+        project.showSnapshotEdit = !project.showSnapshotEdit;
+        project.showDescriptionEdit = !project.showDescriptionEdit;
+        project.showGithubRepoEdit = !project.showGithubRepoEdit;
+    };
 
-  this.updateInfo = function(project) {
-      project.showTitleEdit = !project.showTitleEdit;
-      project.showSnapshotEdit = !project.showSnapshotEdit;
-      project.showDescriptionEdit = !project.showDescriptionEdit;
-      project.showGithubRepoEdit = !project.showGithubRepoEdit;
-};
-  // post updated object to backend
+    this.edit = function(title, briefDescription, description, repoUrl) {
+      this.show = true;
+      this.title = title;
+      this.snapshot = briefDescription;
+      this.description = description;
+      this.repoUrl = repoUrl;
+      $('.client-projects-container').toggleClass('darken');
+    };
+    // post updated object to backend
 
-this.getProjects();
+    this.getProjects();
 
 });
