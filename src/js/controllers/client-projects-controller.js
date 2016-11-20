14887ec6620d8773dc2da3ff0dfd31c7863aca84
Samantha Davis <samantha.shea.davis@gmail.com>
2016-11-20 16:30:34 -0500

    added PUT to update client project

--- src/js/controllers/client-projects-controller.js
+++ src/js/controllers/client-projects-controller.js
@@ -15,6 +15,7 @@ angular.module('codeKarmaApp').controller('ClientProjectsController', function($
         $.ajax(settings).done(function(response) {
             $scope.projects = response;
             $scope.$apply();
+            console.log($scope.projects);
         });
     };
 
@@ -27,14 +28,36 @@ angular.module('codeKarmaApp').controller('ClientProjectsController', function($
         project.showGithubRepoEdit = !project.showGithubRepoEdit;
     };
 
-    this.edit = function(title, briefDescription, description, repoUrl) {
-      this.show = true;
-      this.title = title;
-      this.snapshot = briefDescription;
-      this.description = description;
-      this.repoUrl = repoUrl;
-      $('.client-projects-container').toggleClass('darken');
+    this.edit = function(title, briefDescription, description, repoUrl, id) {
+        this.updateProject = {
+            title: title,
+            brief_description: briefDescription,
+            description: description,
+            github_repo_url: repoUrl
+        };
+        this.id = id;
+        this.show = true;
     };
+
+    this.updateInfo = function() {
+        console.log(this.updateProject);
+
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": this.url,
+            "method": "PUT",
+            "data": this.updateProject
+        };
+
+        $.ajax(settings).done(function(response) {
+            console.log(response);
+            $state.go('codeKarmaParent.clientProjects');
+        });
+
+
+    };
+
     // post updated object to backend
 
     this.getProjects();
