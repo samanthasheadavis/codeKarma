612c05963d56c0e104a62d2ff78810bcfc782bf0
Lindsey Adams <linzma87@gmail.com>
2016-11-22 16:58:19 -0500

    styling comm-feed

--- src/js/controllers/community-feed-controller.js
+++ src/js/controllers/community-feed-controller.js
@@ -5,47 +5,64 @@ angular.module('codeKarmaApp').controller('CommunityFeedController', function($s
 
     this.addPost = function() {
         this.newQuestion = {
-          "karma_question": this.post
+            "karma_question": this.post
         };
         RequestService.postQuestion(this.newQuestion);
         this.post = '';
     };
 
-    this.getPosts = function () {
-      RequestService.getPosts(function(response) {
-        $scope.posts = response;
-        // this.date = $scope.posts.created.slice(0, 10);
-        // this.time = $scope.posts.created.slice(11, 19);
-        // $scope.posts.timeElapsed = this.date + ' ' + this.time;
+    this.getPosts = function() {
+        RequestService.getPosts(function(response) {
+            $scope.posts = response;
+            // this.date = $scope.posts.created.slice(0, 10);
+            // this.time = $scope.posts.created.slice(11, 19);
+            // $scope.posts.timeElapsed = this.date + ' ' + this.time;
 
-        console.log($scope.posts);
-        $scope.$apply();
-      });
+            console.log($scope.posts);
+
+            for (var index = 0; index < $scope.posts.length; index++) {
+
+                if ($scope.posts[index].comments.length > 0) {
+                    var currentPost = $scope.posts[index].comments;
+
+                    for (var commIndex = 0; commIndex < currentPost.length; commIndex++) {
+                      
+                        this.commentDate = currentPost[commIndex].created.slice(0, 10);
+                        this.commentTime = currentPost[commIndex].created.slice(11, 19);
+                        currentPost[commIndex].commentTimestamp = this.commentDate + ' ' + this.commentTime;
+
+                        console.log(currentPost[commIndex].commentTimestamp);
+                    }
+                }
+            }
+
+            $scope.$apply();
+        });
 
     };
 
     this.addResponse = function(comment, id) {
-      // var newResponse = $('<p>').attr('class', 'response').html(comment).appendTo('.responsesContainer');
+        // var newResponse = $('<p>').attr('class', 'response').html(comment).appendTo('.responsesContainer');
 
-      this.newComment = {
-        "karma_comment": comment,
-        "karma_question_id": id
-      };
-      RequestService.postComment(this.newComment);
-      this.response = '';
+        this.newComment = {
+            "karma_comment": comment,
+            "karma_question_id": id
+        };
+        RequestService.postComment(this.newComment);
+        this.response = '';
     };
 
     this.updateLikes = function(likes) {
-      console.log(likes);
-      likes++;
-      this.likes = likes;
+        console.log(likes);
+        likes++;
+        this.likes = likes;
     };
 
     this.getDev = function() {
 
-      RequestService.getDev(function(response) {
-        $scope.currentUser = RequestService.createUser(response.data);
-      });
+        RequestService.getDev(function(response) {
+            $scope.currentUser = RequestService.createUser(response.data);
+        });
     };
 
 
