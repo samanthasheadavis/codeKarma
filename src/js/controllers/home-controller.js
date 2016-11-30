7fd1ac03c8ff9679b605fd02c5c60061992b0370
Samantha Davis <samantha.shea.davis@gmail.com>
2016-11-30 14:48:50 -0500

    refactored home controller

--- src/js/controllers/home-controller.js
+++ src/js/controllers/home-controller.js
@@ -1,5 +1,5 @@
 angular.module('codeKarmaApp').controller('HomeController', function($state) {
-    this.message = "in HomeController";
+
     this.scrollAbout = function() {
         $('html, body').animate({
             scrollTop: $(".about").offset().top
